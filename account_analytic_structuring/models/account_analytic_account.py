6714245ca65c8cd20a31b971ecdb7a0447b8a610
Valentin Thirion <valentinthirion@gmail.com>
2016-10-10 09:12:48 +0200

    [FIX] Corrected file regarding Pylint

--- account_analytic_structuring/models/account_analytic_account.py
+++ account_analytic_structuring/models/account_analytic_account.py
@@ -1,6 +1,7 @@
-from openerp import models, fields, api, report
+from openerp import models, fields, api
 import logging
 _logger = logging.getLogger(__name__)
+
 
 class account_analytic_account(models.Model):
     _inherit = ['account.analytic.account']
