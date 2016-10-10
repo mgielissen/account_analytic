7f610cfd9330b590026f82373c9c4aba5410a3bc
valentinthirion <valentinthirion@gmail.com>
2016-10-10 14:04:30 +0200

    [IMP] hr_analytic_timesheet_improvements Corrected desc file related to guidelines

--- hr_analytic_timesheet_improvements/__openerp__.py
+++ hr_analytic_timesheet_improvements/__openerp__.py
@@ -1,7 +1,9 @@
 {
     'name': "Worklog management improvements",
-    'version': '9.0.1.0',
-    'depends': ['hr_timesheet','project_timesheet', 'project_issue_sheet'],
+    'version': '9.0.1.0.0',
+    'depends': ['hr_timesheet',
+                'project_timesheet',
+                'project_issue_sheet'],
     'author': "Bernard DELHEZ, AbAKUS it-solutions SARL",
     'website': "http://www.abakusitsolutions.eu",
     'category': 'Humain Resources',
@@ -10,10 +12,15 @@
     Worklog management improvements
         
         - It add a calendar view for the worklogs.
-        - date_begin is a new datetime field used to order the worklogs in the calendar. This field is created because the date field from the worklog is a standard date type and we need hours and minutes.
-        - It changes the security rule for multi-company (Analytic Line, Timesheet allowed in read)
+        - date_begin is a new datetime field used to order the
+        worklogs in the calendar. This field is created because
+        the date field from the worklog is a standard date type
+        and we need hours and minutes.
+        - It changes the security rule for multi-company
+        (Analytic Line, Timesheet allowed in read)
 
-    This module has been developed by Bernard DELHEZ @ AbAKUS it-solution.
+    This module has been developed by
+    Bernard DELHEZ @ AbAKUS it-solution.
     """,
     'data': ['view/hr_analytic_timesheet_view.xml',
              'view/project_issue_view.xml',
